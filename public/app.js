3776487cfeff7d03052a8eca6e623b95d1caabcf
Sujal Gautam <sujalgautam747@gmail.com>
2025-02-05 19:44:48 +0545

    Update app.js

--- public/app.js
+++ public/app.js
@@ -5,29 +5,45 @@ const remoteVideo = document.getElementById("remoteVideo");
 let peerConnection;
 const config = { iceServers: [{ urls: "stun:stun.l.google.com:19302" }] };
 
-socket.on("match", (partnerId) => {
-    peerConnection = new RTCPeerConnection(config);
+// Start local video
+navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
+    localVideo.srcObject = stream;
 
-    peerConnection.onicecandidate = (event) => {
-        if (event.candidate) {
-            socket.emit("signal", { to: partnerId, signal: event.candidate });
-        }
-    };
+    socket.on("match", (partnerId) => {
+        console.log("Matched with:", partnerId);
+        peerConnection = new RTCPeerConnection(config);
 
-    peerConnection.ontrack = (event) => {
-        remoteVideo.srcObject = event.streams[0];
-    };
+        stream.getTracks().forEach(track => peerConnection.addTrack(track, stream));
 
-    navigator.mediaDevices.getUserMedia({ video: true, audio: true }).then((stream) => {
-        localVideo.srcObject = stream;
-        stream.getTracks().forEach((track) => peerConnection.addTrack(track, stream));
-        peerConnection.createOffer().then((offer) => peerConnection.setLocalDescription(offer));
+        peerConnection.onicecandidate = (event) => {
+            if (event.candidate) {
+                socket.emit("signal", { to: partnerId, signal: event.candidate });
+            }
+        };
+
+        peerConnection.ontrack = (event) => {
+            remoteVideo.srcObject = event.streams[0];
+        };
+
+        peerConnection.createOffer().then((offer) => {
+            return peerConnection.setLocalDescription(offer);
+        }).then(() => {
+            socket.emit("signal", { to: partnerId, signal: peerConnection.localDescription });
+        });
     });
 
     socket.on("signal", (data) => {
+        if (!peerConnection) return;
+
         if (data.signal.type === "offer") {
             peerConnection.setRemoteDescription(new RTCSessionDescription(data.signal));
-            peerConnection.createAnswer().then((answer) => peerConnection.setLocalDescription(answer));
+            peerConnection.createAnswer().then((answer) => {
+                return peerConnection.setLocalDescription(answer);
+            }).then(() => {
+                socket.emit("signal", { to: data.from, signal: peerConnection.localDescription });
+            });
+        } else if (data.signal.type === "answer") {
+            peerConnection.setRemoteDescription(new RTCSessionDescription(data.signal));
         } else {
             peerConnection.addIceCandidate(new RTCIceCandidate(data.signal));
         }
